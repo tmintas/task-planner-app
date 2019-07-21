46521054dde6150ea0af0b87955488be08b6597d
Timur Frolov <fracass1166@yandex.ru>
2019-07-22 01:48:49 +0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Calendar';
+	private title = 'Calendar';
+
+	constructor() {
+		const a = 5;
+		const b = 2;
+	}
 }
