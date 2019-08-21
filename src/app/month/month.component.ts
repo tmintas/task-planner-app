e5aa66e7e97fbd2b5e0f608a8c7ef6d9138d34a1
Timur Frolov <timur.frolov@arcadia.spb.ru>
2019-08-21 17:25:03 +0300

    Slightly changed layout, added previous month days

--- src/app/month/month.component.ts
+++ src/app/month/month.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-month',
@@ -6,25 +6,35 @@ import { Component, ViewEncapsulation } from '@angular/core';
 	styleUrls: ['./month.component.scss'],
 	encapsulation: ViewEncapsulation.None
 })
-export class MonthComponent {
+export class MonthComponent implements OnInit {
 
 	private monthNumber = 5;
 	private year = 2019;
-	private firstDayDate: Date = new Date(this.year, this.monthNumber, 1);
-	private firstDayPosition = this.firstDayDate.getDay();
-	// private lastMondayOfPreviousMonth =
-	private daysInPrevMonth = new Date(this.year, this.monthNumber - 1, 0).getDate();
+	private firstDayPosition: number = new Date(this.year, this.monthNumber, 1).getDay();
+	private numberOfDaysThisMonth = new Date(this.year, this.monthNumber, 0).getDate();
+	private numberOfDaysPrevMonth = new Date(this.year, this.monthNumber - 1, 0).getDate();
 
-	private get prevMonthLastDays(): number[] {
-		const arr = new Array(this.firstDayPosition - 1);
+	public DisplayDays: number[] = [ ...this.previousMonthLastDays, ...this.currentMonthDays ];
 
-		for (let i = 0; i <= this.firstDayPosition - 2; i++) {
-			arr.push(this.daysInPrevMonth - i);
+	private get currentMonthDays(): number[] {
+		const arr = [];
+
+		for (let i = 1; i <= this.numberOfDaysThisMonth; i++) {
+			arr.push(i);
+		}
+
+		return arr;
+	}
+
+	private get previousMonthLastDays(): number[] {
+		const arr = [];
+
+		for (let i = 1; i < this.firstDayPosition; i++) {
+			arr.push(this.numberOfDaysPrevMonth--);
 		}
 
 		return arr.reverse();
 	}
 
-	public MonthDays: number[] = [ ...this.prevMonthLastDays, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-		19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 ];
+	public ngOnInit() { }
 }
--- src/app/month/month.module.ts
+++ src/app/month/month.module.ts
@@ -4,18 +4,21 @@ import { CommonModule } from '@angular/common';
 import { MonthRoutingModule } from './month-routing.module';
 import { MonthComponent } from './month.component';
 import { DayComponent } from './day/day.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
 	declarations: [
 		MonthComponent,
-		DayComponent
+		DayComponent,
+		CalendarComponent
 	],
 	imports: [
 		CommonModule,
 		MonthRoutingModule
 	],
 	exports: [
-		MonthComponent
+		MonthComponent,
+		CalendarComponent
 	]
 })
 export class MonthModule { }
