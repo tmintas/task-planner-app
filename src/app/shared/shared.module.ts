f445e150466048d4fefae453948b8447385951aa
Timur Frolov <timur.frolov@arcadia.spb.ru>
2019-08-22 18:37:33 +0300

    added font awesome properly

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// font awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fas, far);
+
 @NgModule({
 	declarations: [],
 	imports: [
@@ -8,6 +15,7 @@ import { CommonModule } from '@angular/common';
 	],
 	exports: [
 		CommonModule,
+		FontAwesomeModule
 	]
 })
 export class SharedModule { }
