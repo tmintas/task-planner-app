9fa5b7243238fadd22925833dbb327aececfe6e2
Timur Frolov <timur.frolov@arcadia.spb.ru>
2019-08-22 17:05:45 +0300

    Reverted all icons packages

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
 	declarations: [],
 	imports: [
 		CommonModule,
-		FontAwesomeModule
 	],
 	exports: [
 		CommonModule,
-		FontAwesomeModule
 	]
 })
 export class SharedModule { }
