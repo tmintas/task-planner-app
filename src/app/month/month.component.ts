0ab2f4d3ea4675ad698ab49b74d0b53ee0799a77
Timur Frolov <timur.frolov@arcadia.spb.ru>
2019-09-06 16:09:40 +0300

    Monthdays Outsourced all date functions to separate file

--- src/app/month/month.component.ts
+++ src/app/month/month.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { map } from 'rxjs/operators';
+
 import * as fromDateExtns from '@extensions/date';
 
 @Component({
@@ -22,62 +23,15 @@ export class MonthComponent implements OnInit {
 		return fromDateExtns.GetMonthName(this.monthNumber);
 	}
 
-	private getNextMonthFirstDays() : number[] {
-		const arr = [];
-		const nextMonthFirstDayPosition = fromDateExtns.GetDayOfWeek( new Date(this.year, this.monthNumber + 1, 1) );
-
-		// we don't want to get the whole week of next month if it starts from Monday
-		if (nextMonthFirstDayPosition === 1) { return arr; }
-
-		for (let i = 1; i <= 8 - nextMonthFirstDayPosition; i++) {
-			arr.push(i);
-		}
-
-		return arr;
-	}
-
-	private getCurrentMonthDays() : number[] {
-		const arr = [];
-		const maxDayNumber = fromDateExtns.GetNumberOfDaysInMonth(this.year, this.monthNumber);
-
-		for (let i = 1; i <= maxDayNumber; i++) {
-			arr.push(i);
-		}
-
-		return arr;
-	}
-
-	private getPreviousMonthLastDays() : number[] {
-		const arr = [];
-		const firstDayPosition = fromDateExtns.GetDayOfWeek( new Date(this.year, this.monthNumber, 1) );
-
-		let lastMonthMaxDay = fromDateExtns.GetNumberOfDaysInMonth(this.year, this.monthNumber - 1);
-
-		for (let i = 1; i < firstDayPosition; i++) {
-			arr.push(lastMonthMaxDay--);
-		}
-
-		return arr.reverse();
-	}
-
 	public ngOnInit() : void {
 		console.log(fromDateExtns.GetDayOfWeek(new Date(2019, 7, 1)));
 		this.route.params.pipe(
 			map((prms : Params) => {
 				this.monthNumber = +prms.monthNumber;
 				this.DisplayDays = [
-					...this.getPreviousMonthLastDays(),
-					...this.getCurrentMonthDays(),
-					...this.getNextMonthFirstDays() ];
-
-				console.log('prev days ');
-				console.log(this.getPreviousMonthLastDays());
-
-				console.log('curr days ');
-				console.log(this.getCurrentMonthDays());
-
-				console.log('next days ');
-				console.log(this.getNextMonthFirstDays());
+					...fromDateExtns.GetPreviousMonthLastDays(this.year, this.monthNumber),
+					...fromDateExtns.GetCurrentMonthDays(this.year, this.monthNumber),
+					...fromDateExtns.GetNextMonthFirstDays(this.year, this.monthNumber) ];
 			})
 		).subscribe();
 	}
