7b4b82f77b3d4012403d70a75860319689437562
Timur Frolov <timur.frolov@arcadia.spb.ru>
2020-03-05 19:08:27 +0300

    [Misc] Changed stylings, removed old code

--- ClientApp/src/app/shared/pipes/display-time.pipe.ts
+++ ClientApp/src/app/shared/pipes/display-time.pipe.ts
@@ -7,6 +7,8 @@ import { NgbTime } from '@ng-bootstrap/ng-bootstrap/timepicker/ngb-time';
 export class DisplayTimePipe implements PipeTransform {
 
     transform(time: NgbTime): string {
+        if (!time) return '';
+        
         const minuteText = time.minute < 10 ? `0${time.minute}` : `${time.minute}`;
         return `[${time.hour}:${minuteText}]`;
     }
