127afe678b3794b12e0adb4f9499d2ec6cc59ead
Timur Frolov <timur.frolov@arcadia.spb.ru>
2020-03-05 19:03:15 +0300

    Removed obsolete isLoading logic

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
-import { map } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-root',
@@ -9,18 +7,5 @@ import { map } from 'rxjs/operators';
 	encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-	public IsLoading : boolean = true;
-
-	constructor(private router: Router) {
-		this.router.events.pipe(
-			map((re : Event) => {
-				if (re instanceof NavigationStart) {
-					this.IsLoading = true;
-				}
-				if (re instanceof NavigationEnd || re instanceof NavigationCancel || re instanceof NavigationError) {
-					this.IsLoading = false
-				}
-			})
-		).subscribe();
-	}
+	constructor() { }
 }
